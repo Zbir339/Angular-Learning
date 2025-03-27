2e765374538d0a9010db86c9d64e65406150bab1
Mohamed Amine <amine339@outlook.fr>
2025-03-27 15:23:20 +0000

    Add the onclick functionality using signals

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
